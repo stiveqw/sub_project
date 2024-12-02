907eda48d469530b08e12f307b4f326f279aa8d2
stiveqw <wnahd0002@gmail.com>
2024-12-03 07:55:57 +0900

    1

--- error_service/config.py
+++ error_service/config.py
@@ -1,11 +1,6 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'you-will-never-guess')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql://root:mysql@mysql:3306/festival_db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwt-secret-string')
+    SECRET_KEY = os.environ.get('ERROR_SERVICE_SECRET_KEY')
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
 
--- main_service/config.py
+++ main_service/config.py
@@ -1,11 +1,8 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'you-will-never-guess')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql://root:mysql@mysql:3306/main_db')
+    SECRET_KEY = os.environ.get('MAIN_SERVICE_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = f"mysql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}@{os.environ.get('DB_HOST')}/{os.environ.get('DB_NAME')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwt-secret-string')
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
 
